8540db3f3e2d5d19698ceffe466cac0902c6d329
Tomoki_Seno@0Delta <0deltast@gmail.com>
2015-11-29 14:08:04 +0900

    Shipクラスの雛形を作成

---
+++ Game/Ship.h
@@ -0,0 +1,25 @@
+//==============================================================================
+//	Ship.h
+//		ëDÇêßå‰Ç∑ÇÈÉNÉâÉX
+//==============================================================================
+#pragma once
+#ifndef ___SHIP_HEADER___
+#define ___SHIP_HEADER___
+
+#include"ObjBase.h"
+
+class Ship : public ObjBase {
+
+public:
+	int Init();
+	int Update();
+	int Draw();
+	int Release();
+
+}
+
+
+
+
+#endif
+// EOF
